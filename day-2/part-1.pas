7277242b17f935e622a00e33de1f0b89cc6c87e1
Alberto Lazari <alberto.lazari@gmail.com>
2022-12-04 22:35:16 +0100

    Day 2 - Easy (wrong) solution

--- day-2/part-1.pas
+++ day-2/part-1.pas
@@ -1,7 +1,32 @@
 uses crt;
 
+var
+    opponent_move, player_move, aux: char;
+    score: integer;
+
 begin
-    writeln('hello');
-    readkey;
+    score := 0;
+
+    read(opponent_move);
+    read(aux);
+    read(player_move);
+    
+    while opponent_move <> slinebreak do
+    begin
+        read(aux);
+
+        case opponent_move of
+            'A' : score := score + 8;
+            'B' : score := score + 1;
+            'C' : score := score + 6;
+        end;
+
+        read(opponent_move);
+        read(aux);
+        read(player_move);
+        writeln(opponent_move, player_move);
+    end;
+
+    writeln(score);
 end.
 
--- tools/languages/run-pascal.sh
+++ tools/languages/run-pascal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-fpc part-$1.pas 1> /dev/null && rm part-$1.o
+fpc part-$1.pas 1> compiler.log && rm part-$1.o
 ./part-$1
 
