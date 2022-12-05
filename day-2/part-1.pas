463501ea335afd7b6d5d0e06f3e626a7be5a9505
Alberto Lazari <alberto.lazari@gmail.com>
2022-12-05 12:56:55 +0100

    Day 2 - Part 1

--- day-2/part-1.pas
+++ day-2/part-1.pas
@@ -4,29 +4,49 @@ var
     opponent_move, player_move, aux: char;
     score: integer;
 
+function result (opponent_move, player_move: char): integer;
+(*
+    Returns:
+    1 if player wins
+    0 if there's a draw
+    -1 if opponent wins
+*)
+var
+    o, p : integer;
+
+begin
+    o := integer (opponent_move) - integer ('A');
+    p := integer (player_move) - integer ('X');
+
+    result := (p - o) mod 3;
+    if result < 0 then result := result + 3;
+    if result = 2 then result := -1;
+end;
+
 begin
     score := 0;
 
-    read(opponent_move);
-    read(aux);
-    read(player_move);
+    read (opponent_move);
+    read (aux);
+    read (player_move);
     
-    while opponent_move <> slinebreak do
+    while (opponent_move >= 'A') and (opponent_move <= 'C') do
     begin
-        read(aux);
+        read (aux);
 
-        case opponent_move of
-            'A' : score := score + 8;
-            'B' : score := score + 1;
-            'C' : score := score + 6;
+        case player_move of
+            'X' : score := score + 1;
+            'Y' : score := score + 2;
+            'Z' : score := score + 3;
         end;
 
-        read(opponent_move);
-        read(aux);
-        read(player_move);
-        writeln(opponent_move, player_move);
+        score := score + result (opponent_move, player_move) * 3 + 3;
+
+        read (opponent_move);
+        read (aux);
+        read (player_move);
     end;
 
-    writeln(score);
+    writeln (score);
 end.
 
