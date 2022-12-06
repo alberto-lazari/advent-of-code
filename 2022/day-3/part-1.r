05cab06d7b438b323b67881d66bdb2e80af87aad
Alberto Lazari <alberto.lazari@gmail.com>
2022-12-06 17:43:17 +0100

    Day 3 - Part 2 works

--- 2022/day-3/part-1.r
+++ 2022/day-3/part-1.r
@@ -1,6 +1,22 @@
 rucksacks <- readLines('stdin')
+sum <- 0
 
 for (rucksack in rucksacks) {
+    rucksack <- strsplit(rucksack, '')[[1]]
+    size <- length(rucksack)
+    r.left <- rucksack[1 : (size / 2)]
+    r.right <- rucksack[(size / 2 + 1) : size]
 
+    wrong <- utf8ToInt(intersect(r.left, r.right)[1])
+    print (wrong)
+
+    if (wrong >= utf8ToInt('a')) {
+        sum <- sum + wrong - utf8ToInt('a') + 1
+    }
+    else {
+        sum <- sum + wrong - utf8ToInt('A') + 27
+    }
 }
+
+print(sum)
 
