a8e405be33c503c047d97672a4e579decf1f75e7
Alberto Lazari <alberto.lazari@gmail.com>
2022-12-21 17:00:45 +0100

    Day 6 - Part 1: It probably works

--- 2022/day-6/part-1.clj
+++ 2022/day-6/part-1.clj
@@ -2,10 +2,15 @@
   (loop [sequence (subvec data-buffer 0 sequence-length)
          buffer (subvec data-buffer sequence-length (count data-buffer))
          index sequence-length]
-    (println sequence buffer index)
-    (if (>= (count buffer) 1)
-      (recur (conj sequence (first buffer)) (subvec buffer 1 (count buffer)) (inc index))
+    (if (and
+          (>= (count buffer) 1)
+          ;; the set has not duplicates. If the size is smaller it had duplicates
+          (> (count sequence) (count (set sequence))))
+      (recur
+        (conj (subvec sequence 1 (count sequence)) (first buffer))
+        (subvec buffer 1 (count buffer))
+        (inc index))
       index)))
-  
-(println "result:" (marker [1 2 3 4 5 6 7 8] 4))
+
+(println (marker (clojure.string/split (read-line) #"") 4))
 
