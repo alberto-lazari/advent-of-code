d072ad99e4075eda1a6cd5e6ce333d96656f4cf1
Alberto Lazari <alberto.lazari@gmail.com>
2022-12-21 11:05:00 +0100

    Day 5 - Part 1: It works!

--- 2022/day-5/part-1.pl
+++ 2022/day-5/part-1.pl
@@ -1,17 +1,19 @@
 use warnings;
 use strict;
 
-# split input string in characters
+# split input string in array of characters
 my @input = split //, <STDIN>;
 
 # stacks initialization
 my @stacks;
-for (0..int((scalar @input + 1) / 4) - 1) {
+for (1 .. int ((scalar @input + 1) / 4)) {
     push @stacks, ([]);
 }
 
+# while the final row " 1   2   3..." is not encountered
 while ($input[1] =~ m/[A-Z]| /) {
-    for (0..@stacks - 1) {
+    for (0 .. @stacks - 1) {
+        # index exactly the character representing the crane
         my $crane = $input[$_ * 4 + 1];
         
         if ($crane ne ' ') {
@@ -22,7 +24,27 @@ while ($input[1] =~ m/[A-Z]| /) {
     @input = split //, <STDIN>;
 }
 
-foreach my $stack (@stacks) {
-    print @$stack, "\n";
+# read blank line
+scalar <STDIN>;
+
+for my $input (<STDIN>) {
+    chomp $input;
+    my @step = split / /, $input;
+    
+    my $move = $step[1];
+    my $from = $step[3] - 1;
+    my $to = $step[5] - 1;
+
+    for (1 .. $move) {
+        my $crane = shift @{$stacks[$from]};
+        unshift @{$stacks[$to]}, $crane;
+    }
 }
+
+# print the final result
+for my $stack (@stacks) {
+    print ${$stack}[0];
+}
+
+print "\n";
 
