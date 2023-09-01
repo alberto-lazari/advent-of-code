8d4330899f774ba555fb1330f9dd9d6ce7117e9b
Alberto Lazari <alberto.lazari@gmail.com>
2023-09-01 12:11:57 +0200

    Refactor

---
+++ 2022/day-11/input.erl
@@ -0,0 +1,42 @@
+-module(input).
+-export([starting_items/0, operation_fun/0, test_fun/0]).
+
+% As long as there are commas there are elements to read
+read_items({ok, [","]}, Items) ->
+    {ok, [Item]} = io:fread("", "~d"),
+    read_items(io:fread("", "~s"), Items ++ [Item]);
+
+read_items(_, Items) -> Items.
+
+% Creates the list of the starting items
+starting_items() ->
+    {ok, [Item]} = io:fread("", "  Starting items: ~d"),
+    read_items(io:fread("", "~s"), [Item]).
+
+% Creates the operation function
+operation_fun() ->
+    {ok, [Op]} = io:fread("", " new = old ~c"),
+    {ok, [Val]} = io:fread("", "~s"),
+    fun(Old) ->
+            Value = case Val of
+                        "old" -> Old;
+                        _     -> list_to_integer(Val)
+                    end,
+
+            case Op of
+                "+" -> Old + Value;
+                "*" -> Old * Value
+            end
+    end.
+
+% Creates the test function, that returns the monkey to throw the item to
+test_fun() ->
+    {ok, [ToTest]} = io:fread("", "  Test: divisible by ~d"),
+    {ok, [TrueMonkey]} = io:fread("", "    If true: throw to monkey ~d"),
+    {ok, [FalseMonkey]} = io:fread("", "    If false: throw to monkey ~d"),
+    fun(WorryLevel) ->
+            case WorryLevel rem ToTest of
+                0 -> TrueMonkey;
+                _ -> FalseMonkey
+            end
+    end.
--- 2022/day-11/part-1.erl
+++ 2022/day-11/part-1.erl
@@ -1,44 +1,16 @@
 -module('part-1').
 -export([start/0]).
 
-read_items({ok, [","]}, Items) ->
-    {ok, [Item]} = io:fread("", "~d"),
-    read_items(io:fread("", "~s"), Items ++ [Item]);
-
-read_items(_, Items) -> Items.
+-import(input, [starting_items/0, operation_fun/0, test_fun/0]).
 
 spawn_monkeys() ->
     {ok, [N]} = io:fread("", "Monkey ~d:"),
-    {ok, [Item]} = io:fread("", "  Starting items: ~d"),
-    StartingItems = read_items(io:fread("", "~s"), [Item]),
-
-    {ok, [Op]} = io:fread("", " new = old ~c"),
-    {ok, [Val]} = io:fread("", "~s"),
-    Operation = fun(Old) ->
-                        Value = case Val of
-                                  "old" -> Old;
-                                  _     -> list_to_integer(Val)
-                              end,
-                        case Op of
-                            "+" -> Old + Value;
-                            "*" -> Old * Value
-                        end
-                end,
-    {ok, [ToTest]} = io:fread("", "  Test: divisible by ~d"),
-    {ok, [TrueMonkey]} = io:fread("", "    If true: throw to monkey ~d"),
-    {ok, [FalseMonkey]} = io:fread("", "    If false: throw to monkey ~d"),
-    Test = fun(WorryLevel) -> case WorryLevel rem ToTest of
-                                  0 -> TrueMonkey;
-                                  _ -> FalseMonkey
-                              end
-           end,
-
-    spawn(monkey, start, [N, StartingItems, Operation, Test]),
+    spawn(monkey, start, [N, starting_items(), operation_fun(), test_fun()]),
 
     case io:get_line("") of
         eof -> N + 1;
-        _ -> spawn_monkeys()
+        _   -> spawn_monkeys()
     end.
 
 start() ->
-    spawn_monkeys().
+    Monkeys = spawn_monkeys().
