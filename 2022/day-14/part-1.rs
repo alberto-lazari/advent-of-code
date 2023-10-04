c598da1e34ab0b88f0cd41c940a91e685d7fcea6
Alberto Lazari <alberto.lazari@gmail.com>
2023-10-04 09:27:54 +0200

    Read points

--- 2022/day-14/part-1.rs
+++ 2022/day-14/part-1.rs
@@ -1,7 +1,50 @@
 use std::io::{self, BufRead};
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+struct Path {
+    points: Vec<Point>,
+}
+
+impl Path {
+    fn add_point(&mut self, point: Point) {
+        self.points.push(point)
+    }
+}
+
 fn main() {
+    let mut paths: Vec<Path> = Vec::new();
+
     for line in io::stdin().lock().lines() {
-        println!("{}", line.unwrap());
+        let mut path = Path { points: Vec::new() };
+
+        for point in line.unwrap().split(" -> ") {
+            let mut split = point.split(",");
+            let x: i32 = split.next()
+                              .unwrap()
+                              .parse()
+                              .unwrap();
+            let y: i32 = split.next()
+                              .unwrap()
+                              .parse()
+                              .unwrap();
+
+            path.add_point(Point {
+                x: x,
+                y: y
+            });
+        }
+
+        paths.push(path);
+    }
+
+    for path in paths {
+        for point in path.points {
+            println!("{0},{1}", point.x, point.y);
+        }
+        println!("");
     }
 }
